a8f66f7e51998a7d227fb6cbe9755e8229c0e69d
snguyen212 <nguyen.st@husky.neu.edu>
2016-06-12 23:32:55 -0400

    widget chooser

--- public/assignment/views/Widget/widget-chooser.controller.client.js
+++ public/assignment/views/Widget/widget-chooser.controller.client.js
@@ -5,13 +5,30 @@
         .controller("WidgetChooserController", WidgetChooserController);
 
 
-    function WidgetChooserController($sce) {
+    function WidgetChooserController($location, $routeParams, WidgetService) {
         var vm = this;
+        vm.userId = $routeParams.userId;
+        vm.websiteId = $routeParams.websiteId;
+        vm.pageId = $routeParams.pageId;
+        vm.createWidget = createWidget;
 
+        function createWidget(widgetType) {
+            var widget = {
+                type: widgetType
+            };
 
+            WidgetService
+                .createWidget(vm.pageId, widget)
+                .then(
+                    function (response) {
+                        var widgetId = response.data._id;
+                        $location.url("/user/" + vm.userId + "/website/" + vm.websiteId + "/page/" + vm.pageId + "/widget/" + widgetId);
+                }, 
+                    function (error) {
+                        vm.error = error.data;
+                });
+
+        }
     }
 
-
-
 })();
-
