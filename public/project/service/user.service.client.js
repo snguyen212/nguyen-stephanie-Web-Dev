dd9d79d78beb38de6fc408c89bb1c1b38a7143b1
snguyen212 <nguyen.st@husky.neu.edu>
2016-06-26 05:48:54 -0400

    register

---
+++ public/project/service/user.service.client.js
@@ -0,0 +1,122 @@
+//UPDATED
+
+//factory allows you to create services
+//service is where you find, update, remove any data
+
+
+//MOVE everything to SERVICE side -----
+
+(function () {
+    angular
+        .module("jamn")
+        .factory("UserService", UserService);
+
+    // $http allows you to interact with SERVER
+    function UserService($http) {
+        
+        var users = 
+            [
+                {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
+                {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
+                {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
+                {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
+            ];
+        
+
+        var api = {
+            login: login,
+            createUser: createUser,
+            logout: logout,
+            checkLoggedIn: checkLoggedIn,
+            register: register, //registers creates user AND logs you in
+            findUserByUsernameAndPassword: findUserByUsernameAndPassword,
+            findUserByUsername: findUserByUsername,
+            findUserById: findUserById,
+            updateUser: updateUser,
+            deleteUser: deleteUser
+        };
+        return api;
+        
+        //LOGIN -------------------------------------
+        function login(username, password) {
+            var url = "/api/login";
+            var user = {
+                username: username,
+                password: password
+            };
+            return $http.post(url, user);
+            
+        }
+        
+        function checkLoggedIn() {
+            return $http.get("/api/loggedin");
+        }
+        
+        function register(username, password) {
+            var url = "api/register";
+            var user = {
+                username: username,
+                password: password
+            };
+            return $http.post(url, user);
+            
+        }
+        
+        function logout() {
+           return $http.post("/api/logout");
+        }
+
+        // CREATE USER ----------------------------------------
+        function createUser(username, password) {
+            var url = "/api/user";
+            var newUser = {
+                username: username,
+                password: password
+            };
+            return $http.post(url, newUser);
+
+        }
+
+        // UPDATE USER --------------------------------------
+
+        function updateUser(id, newUser) {
+            // /api/user/:userId is the URL we want from the assignment
+            var url = "/api/user/" + id;
+            //generate PUT
+            //newUser contains info (name, user, pw) of new user
+            return $http.put(url, newUser);
+        }
+
+       
+
+        //DELETE USER -----------------------------------
+            
+//         //will iterate over the id,find the id and delete it
+            function deleteUser(id) {
+                var url = "/api/user" + id;
+                return $http.delete(url);
+            }
+
+        
+
+            //find user by username and password function -------------------------
+            function findUserByUsernameAndPassword(username, password) {
+                var url = "/api/user?username=" + username + "&password=" + password;
+                return $http.get(url);
+            }
+
+            function findUserByUsername(username) {
+                var url = "/api/user?username=" + username;
+                return $http.get(url);
+            }
+        
+
+            function findUserById(id) {
+                var url = "/api/user/" + id;
+                return $http.get(url);
+
+            }
+
+            
+        }
+})();
